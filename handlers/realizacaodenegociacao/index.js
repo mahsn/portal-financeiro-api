d02897973014142d78364a94256b035714d0849f
webjump-nb083 <pedro.esteves@webjump.com.br>
2019-06-19 15:08:59 -0300

    Post to realizacao-de-negociacao endpoint is working.

--- handlers/realizacaodenegociacao/index.js
+++ handlers/realizacaodenegociacao/index.js
@@ -1,16 +1,12 @@
 const repository = require('../../repository/titulosnegociados')
 const RealizacaoDeNegociacao = require('../../models/realizacaodanegociacao')
-const logger = require('morgan')
 const router = require('express').Router()
-
-
 // Handlers are responsible for managing the request and response objects, and link them to a service module that will do the hard work.
 // Each of the following handlers has the req and res parameters, which stands for request and response. 
 // Each handler of this module represents an HTTP verb (GET, POST, PUT and DELETE) that will be linked to them in the future through a router.
 
 router.post('/', (req, res, next) => {
-    console.log(req.body)
-    const [
+    const {
         qtDiasInadimplencia, 
         dsEmail, 
         tpNegociacao, 
@@ -25,7 +21,7 @@ router.post('/', (req, res, next) => {
         nrParcelaLst,
         dtEmissaoLst,
         nrDiasEmAbertoLst
-    ] = req.body
+    } = req.body
 
     const negociacaoData = new RealizacaoDeNegociacao({
         qtDiasInadimplencia,
